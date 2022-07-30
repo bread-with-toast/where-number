a1876c40b0258f950d8a79631bd15d19e65d7954
bread-with-toast <108555960+bread-with-toast@users.noreply.github.com>
2022-07-30 06:08:24 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿int[,] myVeryCoolNumbers = {
+    {1, 2},
+    {3, 4},
+    {5, 6},
+    {7, 8}
+};
+
+Console.WriteLine("What is the first co-ordinate you want (y)?");
+int co1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("What is the seocond co-ordinate you want (x)?");
+int co2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Number at that location is: " + myVeryCoolNumbers[co1, co2]);
